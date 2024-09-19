aa8b3c83a32e1560547eb3a5d4121b8df19717c0
shucheng <shucheng@bd-apaas.com>
2024-09-19 11:03:42 +0800

    v1.3.1

--- .beagle/build.sh
+++ .beagle/build.sh
@@ -2,13 +2,13 @@
 
 set -ex
 
-apk add --no-cache libvirt-dev git
+apk add --no-cache libvirt-dev rsync git
 git config --global --add safe.directory $PWD
 
-export KUBEVIRT_NO_BAZEL=true 
+export KUBEVIRT_NO_BAZEL=true
 export KUBEVIRT_VERSION="${BUILD_VERSION:-v1.3.1}"
 # export KUBEVIRT_GO_BUILD_TAGS="-s -w"
 export KUBEVIRT_RELEASE=true
 
-./hack/build-go.sh install 
+./hack/build-go.sh install
 ./hack/build-copy-artifacts.sh
